23a493c85bfea2944268399a44f1cb9cfe6ffd47
hiroki-nakagawa-ok <gckks439@gmail.com>
2025-05-13 11:30:12 +0900

    解答例

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,18 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            inputText = new TextBox();
+            sendButton = new Button();
+            outputText = new TextBox();
             SuspendLayout();
+            // 
+            // inputText
+            // 
+            inputText.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            inputText.Location = new Point(12, 12);
+            inputText.Multiline = true;
+            inputText.Name = "inputText";
+            inputText.ScrollBars = ScrollBars.Both;
+            inputText.Size = new Size(879, 75);
+            inputText.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            sendButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            sendButton.Location = new Point(897, 12);
+            sendButton.Name = "sendButton";
+            sendButton.Size = new Size(75, 75);
+            sendButton.TabIndex = 1;
+            sendButton.Text = "送信";
+            sendButton.UseVisualStyleBackColor = false;
+            sendButton.Click += sendButton_Click;
+            // 
+            // outputText
+            // 
+            outputText.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            outputText.Location = new Point(12, 93);
+            outputText.Multiline = true;
+            outputText.Name = "outputText";
+            outputText.ScrollBars = ScrollBars.Both;
+            outputText.Size = new Size(960, 456);
+            outputText.TabIndex = 2;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 561);
+            Controls.Add(outputText);
+            Controls.Add(sendButton);
+            Controls.Add(inputText);
             Name = "MainForm";
             Text = "AIとチャット";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox inputText;
+        private Button sendButton;
+        private TextBox outputText;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -6,4 +6,18 @@ public partial class MainForm : Form
     {
         InitializeComponent();
     }
+
+    private async void sendButton_Click(object sender, EventArgs e)
+    {
+        var chat = new ChatCompletionService().StartChat();
+        chat.AddUserMessage(this.inputText.Text);
+
+        this.sendButton.Enabled = false;
+
+        var result = await chat.GetChatMessageContentsAsync();
+
+        this.sendButton.Enabled = true;
+
+        this.outputText.Text = result;
+    }
 }
